142aabd54cdd1fe985946a7aed6c99cabd9be853
HuiSeon Jang <72551674+gguming@users.noreply.github.com>
2021-12-11 22:29:21 +0900

    add file

---
+++ Client-IOS/ColorBookMark/ColorBookMark/Configuration/Extension/UIView.swift
@@ -0,0 +1,18 @@
+//
+//  UIView.swift
+//  ColorBookMark
+//
+//  Created by SUN on 2021/12/11.
+//
+
+import Foundation
+import UIKit
+
+extension UIView{
+    func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        layer.mask = mask
+    }
+}
