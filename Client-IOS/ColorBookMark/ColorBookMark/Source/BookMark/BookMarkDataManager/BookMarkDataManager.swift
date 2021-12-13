072f9956cd5a5bc6a0b0e774341b5fe9674ae8e3
Jihoon701 <92502417+Jihoon701@users.noreply.github.com>
2021-12-13 20:38:31 +0900

    recommit

--- Client-IOS/ColorBookMark/ColorBookMark/Source/BookMark/BookMarkDataManager/BookMarkDataManager.swift
+++ Client-IOS/ColorBookMark/ColorBookMark/Source/BookMark/BookMarkDataManager/BookMarkDataManager.swift
@@ -41,4 +41,35 @@ class BookMarkDataManager {
                 }
             }
     }
+    
+    func getBookMarkInCalendar(date: String, delegate: CalenderViewController) {
+            let header: HTTPHeaders = ["x-access-token" : "\(Constant.jwt!)"]
+
+            AF.request("\(Constant.SERVER_BASE_URL)/app/diarys/targeted-month?date=\(date)", method: .get, parameters: nil, encoding: JSONEncoding(), headers: header)
+                .validate()
+                .responseDecodable(of: GetBookMarkResponse.self) { response in
+                    switch response.result {
+                    case .success(let response):
+                        // 성공했을 때
+                        if response.isSuccess {
+                            delegate.didSuccessGetBookMarkInCalendar(response)
+                        }
+                        // 실패했을 때
+                        else {
+                            switch response.code {
+                            case 2000..<3000: delegate.failedToGetBookMarkInCalendar(message: response.message ?? "" )
+                                print(response.message as Any)
+                            case 3000..<4000: delegate.failedToGetBookMarkInCalendar(message: response.message ?? "" )
+                                print(response.message as Any)
+                            case 4000: delegate.failedToGetBookMarkInCalendar(message: response.message ?? "" )
+                            default: delegate.failedToGetBookMarkInCalendar(message: response.message ?? "" )
+                                print(response.message as Any)
+                            }
+                        }
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                        delegate.failedToGetBookMarkInCalendar(message: "서버와의 연결이 원활하지 않습니다")
+                    }
+                }
+        }
 }
